478472a54e3a9e7c1ee60acf48ae915cf76d9654
uknow-mac <yoon6763@naver.com>
2025-02-15 19:56:20 +0900

    Feat: Github API로 레포지토리 commit 불러오기 기능

--- src/main/kotlin/io/diary/cogi/githubapi/GitHubApiService.kt
+++ src/main/kotlin/io/diary/cogi/githubapi/GitHubApiService.kt
@@ -1,6 +1,8 @@
 package io.diary.cogi.githubapi
 
+import githubtest.dto.commit.CommitItem
 import githubtest.dto.repository.Repository
+import org.apache.commons.logging.LogFactory
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -8,6 +10,8 @@ import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Service
 class GitHubApiService(
@@ -28,6 +32,33 @@ class GitHubApiService(
         val entity = HttpEntity<String>(headers)
 
         val responseType = object : ParameterizedTypeReference<List<Repository>>() {}
+        return restTemplate.exchange(
+            url,
+            HttpMethod.GET,
+            entity,
+            responseType
+        ).body ?: emptyList()
+    }
+
+    fun getCommits(
+        owner: String,
+        repo: String,
+        sinceDate: LocalDateTime = LocalDateTime.now().minusDays(7),
+    ): List<CommitItem> {
+        val parsedSinceDate = sinceDate.format(DateTimeFormatter.ISO_DATE_TIME)
+
+        val url = UriComponentsBuilder.fromUriString(gitHubProperties.baseUrl)
+            .path("/repos/{owner}/{repo}/commits")
+            .queryParam("since", parsedSinceDate)
+            .buildAndExpand(owner, repo)
+            .toUri()
+
+        val headers = HttpHeaders()
+        headers.set("Authorization", "token ${gitHubProperties.token}")
+
+        val entity = HttpEntity<String>(headers)
+
+        val responseType = object : ParameterizedTypeReference<List<CommitItem>>() {}
         return restTemplate.exchange(
             url,
             HttpMethod.GET,
