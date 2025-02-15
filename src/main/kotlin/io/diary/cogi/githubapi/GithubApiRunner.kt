630c6a1d8a3e08cabf1dd0e12dc8c87dabc5c2da
uknow-mac <yoon6763@naver.com>
2025-02-15 23:56:25 +0900

    Feat: GithubApiRunner 추가

---
+++ src/main/kotlin/io/diary/cogi/githubapi/GithubApiRunner.kt
@@ -0,0 +1,30 @@
+package io.diary.cogi.githubapi
+
+import org.apache.commons.logging.LogFactory
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+
+@Component
+class GithubApiRunner(
+    private val githubProperties: GitHubProperties,
+    private val gitHubApiService: GitHubApiService,
+) : ApplicationRunner {
+
+    private val log = LogFactory.getLog(GithubApiRunner::class.java)
+
+    override fun run(args: ApplicationArguments?) {
+        val repositories = gitHubApiService.getAllRepositories(githubProperties.username)
+
+        repositories.forEach { repository ->
+            val commits = gitHubApiService.getCommits(githubProperties.username, repository.name)
+            log.info("Repository: ${repository.name}")
+            if (commits.isEmpty()) {
+                log.info("-- No commits found --")
+            }
+            commits.forEach { commit ->
+                log.info("Commit: ${commit.commit.message}")
+            }
+        }
+    }
+}
