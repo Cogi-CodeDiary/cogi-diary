1905c89d68fc4fdaa10474629e367af35eb10400
uknow-mac <yoon6763@naver.com>
2025-02-16 00:37:09 +0900

    Refactor: GitHubApiRunner 코틀린 코루틴을 사용한 병렬 처리 개선

--- src/main/kotlin/io/diary/cogi/githubapi/GitHubApiRunner.kt
+++ src/main/kotlin/io/diary/cogi/githubapi/GitHubApiRunner.kt
@@ -5,6 +5,8 @@ import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
 
+import kotlinx.coroutines.*
+
 @Component
 class GitHubApiRunner(
     private val githubProperties: GitHubProperties,
@@ -16,14 +18,19 @@ class GitHubApiRunner(
     override fun run(args: ApplicationArguments?) {
         val repositories = gitHubApiService.getAllRepositories(githubProperties.username)
 
-        repositories.forEach { repository ->
-            val commits = gitHubApiService.getCommits(githubProperties.username, repository.name)
-            log.info("Repository: ${repository.name}")
-            if (commits.isEmpty()) {
-                log.info("-- No commits found --")
-            }
-            commits.forEach { commit ->
-                log.info("Commit: ${commit.commit.message}")
+        runBlocking {
+            val repositoriesWithCommits = repositories.map { repository ->
+                async {
+                    val commits = gitHubApiService.getCommits(githubProperties.username, repository.name)
+                    repository to commits
+                }
+            }.awaitAll().toMap()
+
+            repositoriesWithCommits.forEach { (repository, commits) ->
+                log.info("Repository: ${repository.name}")
+                commits.forEach { commit ->
+                    log.info("  - ${commit.commit.message} ${commit.commit.author.date}")
+                }
             }
         }
     }
