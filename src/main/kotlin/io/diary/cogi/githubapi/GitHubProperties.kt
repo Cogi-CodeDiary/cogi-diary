16cc5b9de7e49f263ffce0e3fdf052a56dec7a44
uknow-mac <yoon6763@naver.com>
2025-02-15 19:49:10 +0900

    Feat: GitHubProperties 설정 클래스 추가 (application.yml으로 부터 읽음)

---
+++ src/main/kotlin/io/diary/cogi/githubapi/GitHubProperties.kt
@@ -0,0 +1,12 @@
+package io.diary.cogi.githubapi
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "github")
+class GitHubProperties(
+    var username: String = "",
+    var token: String = "",
+    var baseUrl: String = "https://api.github.com",
+)
