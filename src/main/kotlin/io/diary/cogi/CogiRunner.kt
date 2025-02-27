75fcae5c2a0b2652958dacfa5d9a73ea1e36e086
K-tuna <ehddnjs0710@gmail.com>
2025-02-28 02:39:17 +0900

    Feat: 예제 커밋 메시지 리스트로 제미나이 request 생성 후 generatorService 사용해서 호출 로직

--- src/main/kotlin/io/diary/cogi/CogiRunner.kt
+++ src/main/kotlin/io/diary/cogi/CogiRunner.kt
@@ -1,5 +1,7 @@
 package io.diary.cogi
 
+import io.diary.cogi.generator.GeneratorRequestDto
+import io.diary.cogi.generator.GeneratorService
 import org.apache.commons.logging.LogFactory
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
@@ -7,11 +9,37 @@ import org.springframework.stereotype.Component
 
 @Component
 class CogiRunner(
+    private val generatorService: GeneratorService
 ) : ApplicationRunner {
 
     private val log = LogFactory.getLog(CogiRunner::class.java)
 
     override fun run(args: ApplicationArguments?) {
         log.info("Hello, Cogi!")
+
+        // 예제 커밋 메시지 리스트
+        val commitMessages = listOf(
+            "Implemented user login functionality",
+            "Fixed a bug where the password reset link was not working",
+            "Refactored database connection handling",
+            "Updated UI design for better user experience"
+        )
+
+        // 제미나이 request 생성
+        val requestDto = GeneratorRequestDto(
+            contents = listOf(
+                GeneratorRequestDto.Content(
+                    parts = GeneratorRequestDto.Parts(commitMessages.joinToString("\n"))
+                )
+            ),
+            generationConfig = GeneratorRequestDto.GenerationConfig(1,1000,0.7)
+        )
+
+        // GeneratorService 호출
+        val response = generatorService.processPrompt(requestDto)
+
+        // 결과
+        log.info("Generated Diary: ${response.candidates}")
+
     }
 }
